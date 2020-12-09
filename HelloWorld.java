0fee85333601d4db8321ec1b9e68524a6ff33ce2
Sanjan Geet Singh <sanjangeet720@gmail.com>
2020-12-09 09:43:43 +0530

    Added 'HelloWorld.java'

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Prints "Hello World!" on the screen without quotes.
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
